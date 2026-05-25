bb48c7d439be323063fbb15d395f23e6dcd2967a
mdb200806 <abe.mdb200806@gmail.com>
2026-05-25 19:30:30 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+
+print("環境設定が完了しました！")
+print("現在のPythonの場所:", sys.executable)
+
+import yfinance as yf
+
+# 三菱HCキャピタル（8593.T）の株価を取得
+ticker = "8593.T"
+stock = yf.Ticker(ticker)
+price = stock.history(period="1d")['Close'].iloc[-1]
+
+print(f"{ticker} の現在価格: {price:.1f}円")
